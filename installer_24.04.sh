862cb6f6d50acf81a8784c88c683018a71aef0d5
nu11secur1ty <venvaropt@gmail.com>
2024-06-03 12:04:03 +0300

    Update installer_24.04.sh

--- installer_24.04.sh
+++ installer_24.04.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/pash
-apt-get update
-apt-get install ca-certificates curl
+apt-get update -y
+apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -10,7 +10,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
+apt-get update -y
 
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
