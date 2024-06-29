fd759dbc3403ead04508f4f109b181b2d77b9be8
nu11secur1ty <venvaropt@gmail.com>
2024-06-29 08:04:55 +0300

    Create uninstaller.sh

---
+++ uninstaller.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo "### Cleaning your OS..."
+echo
+sleep 5;
+# Clean and prepare the ENV
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+rm -rf /var/lib/docker
+rm -rf /etc/docker
