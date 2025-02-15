4dfbc28afe4afbe8287f6eadd6f87cc115caf4e4
nu11secur1ty <venvaropt@gmail.com>
2025-02-15 19:51:31 +0200

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -8,7 +8,7 @@ docker stop $(docker ps -a -q) docker rm $(docker ps -a -q) docker rmi $(docker 
 systemctl stop docker.service
 sudo apt-get remove docker-compose-plugin -y
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-systemctl stop docker.service
-systemctl stop docker.socket
+# systemctl stop docker.service
+# systemctl stop docker.socket
 rm -rf /var/lib/docker/
 rm -rf /etc/docker/
