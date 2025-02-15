d5ef02997245c564489d265c29fdf7cb94d48b82
nu11secur1ty <venvaropt@gmail.com>
2025-02-15 18:43:34 +0200

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -4,6 +4,8 @@ echo "### Cleaning your OS..."
 echo
 sleep 5;
 # Clean and prepare the ENV
+systemctl stop docker.service
+systemctl stop docker.socket
 sudo apt-get remove docker-compose-plugin -y
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 systemctl stop docker.service
